c2ce238cc16e0e97a8ffbb0ffb89723352bc13ef
keerthiram2612 <keerthiram2612@gmail.com>
2022-11-08 07:55:44 +0530

    updating my files in new branch

---
+++ fahrenheit.py
@@ -0,0 +1,6 @@
+F=float(input("Enter the temperature in fahrenheit:"))
+C=(F-32)*5.0/9.0
+print(F,"in Farenheit =",C,"in celsius")
+C=float(input("Enter temperature in celsius:"))
+F=9.0/5.0*C+32
+print(C,"in celsius=",F,"in Fahrenheit")
