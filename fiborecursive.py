e8f8d6328dc82970788297b5eaa465d9d63ba342
keerthiram2612 <keerthiram2612@gmail.com>
2022-11-21 20:58:56 +0530

    adding file to python file

---
+++ fiborecursive.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n==0: #base class
+        return 0
+    elif n==1: #base class
+        return 1 
+    else:
+        return fib(n-1) + fib(n-2)  #recursive fibonacci function call
+nterms = int(input("How many terms?"))
+if nterms<=0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequences:")
+    for i in range(nterms):
+        print(fib(i))
