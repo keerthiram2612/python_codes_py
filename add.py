37d6beae08ca09fbb50c17e58ecd8fd07b17396b
keerthiram2612 <keerthiram2612@gmail.com>
2022-12-10 12:58:23 +0530

    adding the file to the pythonfile

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
 def display_info(first_name,last_name):
     print('First Name:' ,first_name)
     print('last Name:', last_name)
-display_info(last_name= 'Cartman',first_name='Eric')
+display_info(last_name= 'Cartman',first_name='keerthana')
